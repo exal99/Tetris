7105e8dba27b3cf2bc44c5daef9ecff1b56f82b9
Alexander Simko <asimko94@gmail.com>
2016-09-19 17:32:16 +0200

    Made the gui work

--- src/gui/GameGUI.java
+++ src/gui/GameGUI.java
@@ -1,39 +1,35 @@
 package gui;
 
-import java.awt.Frame;
-import java.awt.Graphics;
-
 import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 
 import gameBoard.GameBoard;
 
-public class GameGUI extends Frame{
+public class GameGUI{
 	
 	private FieldGUI field;
 	private InfoGUI info;
-
-	private static final long serialVersionUID = 1L;
+	private JFrame root;
 	
 	public GameGUI(GameBoard g) {
 		super();
+		root = new JFrame("Tetris");
 		field = new FieldGUI(g);
 		info = new InfoGUI(g, field.getSquareSize(), field.getXPadding(), field.getYPadding());
-		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		add(info);
-		add(field);
+		root.setLayout(new BoxLayout(root.getContentPane(), BoxLayout.X_AXIS));;
+		root.add(info);
+		root.add(field);
+		root.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	@Override
-	public void update(Graphics g) {
-		info.update(g);
-		field.update(g);
-		super.update(g);
+	public void update() {
+		info.repaint();
+		field.repaint();
+		root.repaint();
 	}
 	
-	@Override
-	public void paint(Graphics g) {
-		info.paint(g);
-		field.paint(g);
+	public JFrame getRoot() {
+		return root;
 	}
 	
 
