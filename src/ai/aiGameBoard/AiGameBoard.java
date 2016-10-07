42e3fb8e81e1b49a8da651e3f02e9d3e0b66ecbf
Alexander Simko <asimko94@gmail.com>
2016-10-07 17:59:33 +0200

    implemented AiGameBoard.clone()

--- src/ai/aiGameBoard/AiGameBoard.java
+++ src/ai/aiGameBoard/AiGameBoard.java
@@ -16,7 +16,26 @@ public class AiGameBoard extends GameBoard {
 	}
 	
 	public AiGameBoard clone() {
-		return null;
+		/**
+		 * Returns a clone of the AiGameBoard. The clone cannot be used in place of the parent
+		 * since the clone isn't created as a perfect clone but rather as a clone used for simulating all
+		 * possibilities. It contains all the necessary information to determine the best possible move for 
+		 * the game board.
+		 * 
+		 * @return a clone of the AiGameBoard
+		 */
+		AiGameBoard clone = new AiGameBoard(null);
+		clone.combo = combo;
+		clone.controlling = controlling;
+		clone.field = field;
+		clone.hold = hold;
+		clone.level = level;
+		clone.paused = paused;
+		clone.queue = queue;
+		clone.running = running;
+		clone.score = score;
+		clone.typeField = typeField;
+		return clone;
 	}
 	
 	public boolean checkValidState(int deltaX, int deltaY) {
