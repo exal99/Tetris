b9e09ae33550ea15c4301b307b49a9cd04b54f4a
Alexander Simko <asimko94@gmail.com>
2016-10-10 20:56:51 +0200

    implemented ai game loop

--- src/ai/game/MainAiGameThread.java
+++ src/ai/game/MainAiGameThread.java
@@ -1,21 +1,50 @@
 package ai.game;
 
-import java.util.Timer;
 
 import ai.aiGameBoard.AiGameBoard;
+import main.gui.GameGUI;
 
 public class MainAiGameThread extends Thread {
 	
 	private AiGameBoard game;
-	private Timer timer;
+	private static boolean updateGraphics;
+	private static GameGUI graphics;
 	
-	public MainAiGameThread(Timer t, AiGameBoard g) {
-		timer = t;
+	public MainAiGameThread(AiGameBoard g) {
 		game = g;
+	}
+	
+	public static void setGraphicsUpdate(boolean value) {
+		updateGraphics = value;
+	}
+	
+	public static void setGraphics(GameGUI newGraphics) {
+		setGraphicsUpdate(true);
+		graphics = newGraphics;
 	}
 	
 	@Override
 	public void run() {
+		long lastFrame = System.nanoTime();
+		long lastTick = System.nanoTime();
+		long lastUpdate = System.nanoTime();
+		final long FPS = 60;
+		final long TICKS = 1200;
 		
+		while (isAlive() && !isInterrupted()) {
+			if (updateGraphics && System.nanoTime() - lastFrame >= 1000000000/FPS) {
+				if (graphics != null) {
+					graphics.update();
+				}
+				lastFrame = System.nanoTime();
+			}
+			if (System.nanoTime() - lastTick >= 1000000000/TICKS) {
+				if (System.nanoTime() - lastUpdate >= 60/(game.getGravity()*1200) * 1000000000 && !game.isPaused() && game.isRuning()) {
+					game.update();
+					lastUpdate = System.nanoTime();
+				}
+				lastTick = System.nanoTime();
+			}
+		}
 	}
 }
